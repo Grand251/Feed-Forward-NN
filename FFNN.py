72b6b4408e51de04773ffddfafb7836163221207
Grand251 <austinrm66@gmail.com>
2018-07-29 20:44:16 +0200

    Switched to Stochastic Learning

--- FFNN.py
+++ FFNN.py
@@ -24,9 +24,6 @@ class FFNN:
 
 
 	def build_model(self, X, Y, layer_info, num_passes):
-		#np.random.seed(0)
-
-		#Not counting hidden layer
 		num_layers = len(layer_info) - 1
 		weights = list(range(num_layers))
 		bias = list(range(num_layers))
@@ -38,34 +35,42 @@ class FFNN:
 		#0 Will account for input layer
 		outs = list(range(num_layers + 1))
 		nets = list(range(num_layers + 1))
-		outs[0] = X
-		nets[0] = X
-	
+		training_example = np.zeros((1, len(X[0])))
+		training_label = np.zeros((1, len(Y[0])))
+		
+		#Pass through the entire dataset
 		for it in range(num_passes):
-			layer_input = X
-			for i in range(1, num_layers + 1):
-				nets[i] = layer_input.dot(weights[i - 1]) + bias[i - 1]
-				outs[i] = np.tanh(nets[i])
-				layer_input = outs[i]
+			
+			#One training example at a time
+			for t in range(len(X)):
+				training_example[0] = X[t]
+				training_label[0] = Y[t]
+				layer_input = training_example
+				outs[0] = training_example
+				nets[0] = training_example
+				for i in range(1, num_layers + 1):
+					nets[i] = layer_input.dot(weights[i - 1]) + bias[i - 1]
+					outs[i] = np.tanh(nets[i])
+					layer_input = outs[i]
 	
-			#Backpropagation
-			learning_rate = 0.1
-			error = layer_input - Y
+				#Backpropagation
+				learning_rate = 0.01
+				error = layer_input - training_label
 	
-			dWeights = list(range(num_layers))
-			dBias = list(range(num_layers))
-			delta = list(range(num_layers))
+				dWeights = list(range(num_layers))
+				dBias = list(range(num_layers))
+				delta = list(range(num_layers))
 	
-			delta[num_layers - 1] = error
-	
-			for i in list(range(num_layers))[::-1]:
-				dWeights[i] = (outs[i].T).dot(delta[i])
-				dBias[i] = np.sum(delta[i], axis=0)
-				if i > 0:
-					delta[i -1] = delta[i].dot(weights[i].T) * (1 - np.power(outs[i], 2))
-			for i in range(num_layers):
-				weights[i] -= learning_rate * dWeights[i]
-				bias[i] -= learning_rate * dBias[i]
+				delta[num_layers - 1] = error
+		
+				for i in list(range(num_layers))[::-1]:
+					dWeights[i] = (outs[i].T).dot(delta[i])
+					dBias[i] = np.sum(delta[i], axis=0)
+					if i > 0:
+						delta[i -1] = delta[i].dot(weights[i].T) * (1 - np.power(outs[i], 2))
+				for i in range(num_layers):
+					weights[i] -= learning_rate * dWeights[i]
+					bias[i] -= learning_rate * dBias[i]
 		
 		#Save weights and bias to the object
 		self.model = {'weights': weights, 'bias': bias}
